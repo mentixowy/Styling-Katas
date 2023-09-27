2ba1aa89c6c995b0149f22a5fc1080122f49711d
Kamil Mentel <kamilmentel@MacBook-Pro-Kamil.local>
2023-09-27 17:22:54 +0200

    Example of message

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ export default function App() {
             <li>1</li>
             <li>2</li>
             <li>3</li>
+            <li>3</li>
+            <li>3</li>
+            <li>3</li>
           </ul>
         </nav>
       </header>
