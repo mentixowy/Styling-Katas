83918226abce8b639ea789d30642bbc20bbae569
Kamil Mentel <kamilmentel@mbp-kamil.home>
2023-09-27 23:05:14 +0200

    Add basic variables such as font, colors and logo

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ export default function App() {
   return (
     <main>
       <header className="header">
-        <nav>
-          <ol>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>3</li>
-            <li>3</li>
-            <li>3</li>
-          </ol>
-        </nav>
+        <p>This is example text</p>
       </header>
     </main>
   );
