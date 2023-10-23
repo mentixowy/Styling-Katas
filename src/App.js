5ad4d4dd4d1ef100395632c83cc87c62af563abd
Kamil Mentel <kamilmentel@MacBook-Pro-Kamil.local>
2023-10-23 19:40:01 +0200

    Adding requested changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import { AboutUs } from "./AbousUs";
 import { OurValues } from "./OurValues";
 
-// export default function App() {
-//   return <AboutUs />;
-//   return <OurValues />;
-// }
 function App() {
   return (
     <div className="App">
